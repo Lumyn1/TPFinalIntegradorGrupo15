d4dc1e66cafb8757abc1cc5d7242938e9aad49ea
Lumyn1 <lucianomartin2002@hotmail.com>
2026-06-13 00:17:47 -0300

    Añadidos punto 1 y 2 del modulo A LucianoRiveros

---
+++ src/data/usuarios.js
@@ -0,0 +1,17 @@
+// Simulamos una base de datos de usuarios autorizados
+export const usuariosDb = [
+    { 
+        id: 1, 
+        nombre: "Carlos Gerente", 
+        usuario: "admin", 
+        password: "123", 
+        rol: "Gerencia" 
+    },
+    { 
+        id: 2, 
+        nombre: "Laura Soporte", 
+        usuario: "soporte", 
+        password: "123", 
+        rol: "Soporte" 
+    }
+];
